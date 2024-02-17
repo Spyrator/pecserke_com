24db4e9551c22b7d8aa05a0dcb7faf35988ce5d8
Filip Pecsérke <filip.pecserke@gmail.com>
2024-02-17 20:08:03 +0100

    Cleanup, removed Turso, because it's not ready for WASM

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-#![allow(unused)]
+// #![allow(unused)]
 
 mod handlers;
 
-mod auth;
-
-// use auth::{login, user, users};
 use axum::{
     routing::{get, post},
     Router,
@@ -15,26 +12,11 @@ use handlers::{
     qr::get_qr,
     settings::get_settings,
 };
-use libsql::Connection;
+
 use shuttle_axum::ShuttleAxum;
-use shuttle_secrets::SecretStore;
 
 #[shuttle_runtime::main]
-async fn init(
-    #[shuttle_secrets::Secrets] secrets: SecretStore,
-    #[shuttle_turso::Turso(
-        addr = "{secrets.TURSO_LOCAL_ADDR}",
-        local_addr = "{secrets.TURSO_LOCAL_ADDR}",
-        token = "{secrets.TURSO_TOKEN}"
-    )]
-    client: Connection,
-) -> ShuttleAxum {
-    //let state = Arc::new(Mutex::new(client));
-
-    // let rocket = rocket::build()
-    //     .manage(client)
-    //     .mount("/auth/", routes![login, user, users])
-
+async fn init() -> ShuttleAxum {
     let router = Router::new()
         .route("/", get(get_index))
         .route("/settings", get(get_settings))
@@ -42,6 +24,5 @@ async fn init(
         .route("/payme/:amount", get(pay_me_amount))
         .route("/payme/:amount/:message", get(pay_me_amount_message))
         .route("/payme/form", post(pay_me_form));
-
     Ok(router.into())
 }
