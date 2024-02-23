bfeab9e8191df3d7fad40c8414c4c68961798b5a
Filip Pecsérke <filip.pecserke@gmail.com>
2024-02-23 14:23:25 +0100

    fixed templating and adde hx-boost

--- src/handlers/qr.rs
+++ src/handlers/qr.rs
@@ -1,13 +1,10 @@
 use askama::Template;
 use axum::response::Html;
 
-use super::template::WebsiteTemplate;
-
 #[derive(Template)]
 #[template(path = "qr.html")]
 pub struct QrTemplate {}
 
 pub async fn get_qr() -> Html<String> {
-    let qr = QrTemplate {}.to_string();
     return Html(QrTemplate {}.to_string());
 }
--- src/handlers/settings.rs
+++ src/handlers/settings.rs
@@ -1,13 +1,10 @@
 use askama::Template;
 use axum::response::Html;
 
-use super::template::WebsiteTemplate;
-
 #[derive(Template)]
 #[template(path = "settings.html")]
 pub struct SettingsTemplate {}
 
 pub async fn get_settings() -> Html<String> {
-    let settings = SettingsTemplate {}.to_string();
     return Html(SettingsTemplate {}.to_string());
 }
